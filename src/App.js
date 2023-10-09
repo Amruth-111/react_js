f9087190d29eb31fc2e989963baf1ece9cb9b897
Amruth <amruthkrishnaa@gmail.com>
2023-10-09 13:36:37 +0530

    props

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+let content="Legendary Amruth"
+function App() {
+  return (
+    <>
+    <Navbar title="TextUtils" about="About"></Navbar>
+    {/* <Navbar></Navbar> */}
+    </>
+  );
+}
+
+export default App;
